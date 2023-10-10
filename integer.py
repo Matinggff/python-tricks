2e6e29f14f23bd8ccc668f150cdd292ff13d0534
Matin Salehi <119169794+Matinggff@users.noreply.github.com>
2023-10-10 18:33:50 +0330

    Update integer.py

--- integer.py
+++ integer.py
@@ -2,5 +2,5 @@ number1 = 7_000_000_000
 number2 = 3_000
 
 ans = number1*number2
-print(f'{ans}') 
-print(f'{ans:_}') 
+print(f'{ans}') # output --> 21000000000000
+print(f'{ans:_}') # output --> 21_000_000_000_000
