911bd50e5679a007502264aed41697925550f935
Matin Salehi <119169794+Matinggff@users.noreply.github.com>
2023-10-10 18:27:49 +0330

    Update round.py

--- round.py
+++ round.py
@@ -1,7 +1,7 @@
 number = 257.656
 
-print(round(number, 2))
-print(round(number, 1))
-print(round(number, 0))
-print(round(number, -1))
-print(round(number, -2))
+print(round(number, 2)) # output --> 257.66
+print(round(number, 1)) # output --> 257.7
+print(round(number, 0)) # output --> 258.0
+print(round(number, -1)) # output --> 260.0
+print(round(number, -2)) # output --> 300.0
