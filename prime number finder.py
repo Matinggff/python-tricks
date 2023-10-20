258be065e24ba0d145c8a4d54da9abeefc28a2a8
Matin Salehi <119169794+Matinggff@users.noreply.github.com>
2023-10-20 22:02:56 +0330

    add prime number finder

---
+++ prime number finder.py
@@ -0,0 +1,12 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+N = 50  
+prime_numbers = [num for num in range(2, N + 1) if is_prime(num)]
+
+print("number 1 to", N, ":", prime_numbers)
